d7287394edf2055c85207ffc93a4ef631035084b
Pyachi <snglass2002@hotmail.com>
2021-11-02 15:33:58 -0500

    a

--- src/texture.cpp
+++ src/texture.cpp
@@ -3,11 +3,11 @@
 Texture::Texture(const QString& texture, int zValue)
 		: texture(texture), zValue(zValue) {}
 
-const Texture Texture::PLAYERHITBOX = Texture(":/images/player_hitbox.png", 4);
-const Texture Texture::PLAYER1 = Texture(":/images/player.png", 3);
-const Texture Texture::ENEMY1 = Texture(":/images/enemy.png", 3);
+const Texture Texture::PLAYERHITBOX = Texture(":/images/images/player_hitbox.png", 4);
+const Texture Texture::PLAYER1 = Texture(":/images/images/player.png", 3);
+const Texture Texture::ENEMY1 = Texture(":/images/images/enemy.png", 3);
 const Texture Texture::PLAYERBULLETBASIC =
-		Texture(":/images/player_bullet.png", 2);
-const Texture Texture::BULLETROUND = Texture(":/images/bulletRound.png", 2);
-const Texture Texture::BULLETELLIPSE = Texture(":/images/bulletEllipse.png", 2);
-const Texture Texture::BACKGROUND = Texture(":/images/background.png", 5);
+		Texture(":/images/images/player_bullet.png", 2);
+const Texture Texture::BULLETROUND = Texture(":/images/images/bulletRound.png", 2);
+const Texture Texture::BULLETELLIPSE = Texture(":/images/images/bulletEllipse.png", 2);
+const Texture Texture::BACKGROUND = Texture(":/images/images/background.png", 5);
