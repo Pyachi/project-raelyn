97dacc80dc83a09caa79cd1399818ea0559d6ae7
spooky <gsw0005@uah.edu>
2021-11-09 17:28:26 -0600

    Added enemy types and added to levelone.txt

--- src/ai/enemy.cpp
+++ src/ai/enemy.cpp
@@ -14,6 +14,7 @@ EntityEnemy* spawn(Enemy enemy, UUID uuid, const QPointF& loc) {
           enemy->fireBullets(PATTERN_SHOTGUN10X5);
       });
       break;
+    //***********************************************************************************
     case ENEMY_ENEMYTEST2:
       entity = new EntityEnemy(TEXENEMY1, uuid, 100, [](EntityEnemy* enemy) {
         if (enemy->getAge() < 60)
@@ -23,6 +24,7 @@ EntityEnemy* spawn(Enemy enemy, UUID uuid, const QPointF& loc) {
           enemy->deleteLater();
       });
       break;
+    //************************************************************************************
     case ENEMY_SIMPLESHOTGUN_TOP_LEFT:
       entity = new EntityEnemy(TEXENEMY1, uuid, 5, [](EntityEnemy* enemy) {
         if (enemy->cycle(1800, 1, 150))
@@ -39,8 +41,9 @@ EntityEnemy* spawn(Enemy enemy, UUID uuid, const QPointF& loc) {
           enemy->deleteLater();
       });
       break;
+    //************************************************************************************
     case ENEMY_SIMPLESHOTGUN_TOP_RIGHT:
-      entity = new EntityEnemy(TEXENEMY1, uuid, 5, [](EntityEnemy* enemy) {
+      entity = new EntityEnemy(TEXENEMY1, uuid, 15, [](EntityEnemy* enemy) {
         if (enemy->cycle(1800, 1, 150))
           enemy->moveBy(-2, -2);
         else if (enemy->cycle(1800, 151, 250))
@@ -55,20 +58,23 @@ EntityEnemy* spawn(Enemy enemy, UUID uuid, const QPointF& loc) {
           enemy->deleteLater();
       });
       break;
+    //***************************************************************************************
     case ENEMY_TANKYSHOTGUN_UP_DOWN:
       entity = new EntityEnemy(TEXENEMY1, uuid, 100, [](EntityEnemy* enemy) {
         if (enemy->cycle(1800, 1, 175))
-          enemy->moveBy(0, .5);
+          enemy->moveBy(0, 1.75);
         if (enemy->cycle(1800, 176, 350))
-          enemy->moveBy(0, -.5);
-        if (enemy->cycle(1800, 351, 700))
-          enemy->moveBy(0, .5);
+          enemy->moveBy(0, -1);
+        if (enemy->cycle(1800, 351, 525))
+          enemy->moveBy(0, 1);
+        if (enemy->cycle(1800, 526, 700))
+          enemy->moveBy(0, -1);
+        if (enemy->cycle(1800, 701, 875))
+          enemy->moveBy(0, 1);
         if (enemy->cycle(1800, 876, 1050))
-          enemy->moveBy(0, -.5);
-        if (enemy->cycle(1800, 1226, 1400))
-          enemy->moveBy(0, .5);
-        if (enemy->cycle(1800, 1401, 1800))
-          enemy->moveBy(0, 2);
+          enemy->moveBy(0, -1);
+        if (enemy->cycle(1800, 1051, 1800))
+          enemy->moveBy(0, -3);
 
         if (enemy->cycle(350, 175))
           enemy->fireBullets(PATTERN_SHOTGUN8X5);
@@ -76,6 +82,60 @@ EntityEnemy* spawn(Enemy enemy, UUID uuid, const QPointF& loc) {
         if (enemy->getAge() == 1900)  // 15 seconds plus off screen
           enemy->deleteLater();
       });
+      break;
+    //****************************************************************************************
+    case ENEMY_SIMPLESHOTGUN_MID_LEFT_UP:
+      entity = new EntityEnemy(TEXENEMY1, uuid, 15, [](EntityEnemy* enemy) {
+        if (enemy->cycle(1800, 1, 120))
+          enemy->moveBy(2.5, 0);
+        if (enemy->cycle(1800, 160, 240))
+          enemy->moveBy(0, -2);
+        if (enemy->cycle(1800, 280, 900))
+          enemy->moveBy(-2.75, -2.75);
+
+        if (enemy->getAge() == 159) {
+          enemy->fireBullets(PATTERN_SHOTGUN8X3, 180);
+          enemy->fireBullets(PATTERN_SHOTGUN8X3);
+        }
+        if (enemy->getAge() == 279)
+          enemy->fireBullets(PATTERN_SHOTGUN8X3);
+
+        if (enemy->getAge() == 900)  // 15 seconds plus off screen
+          enemy->deleteLater();
+      });
+      break;
+    //***************************************************************************************
+    case ENEMY_SIMPLESHOTGUN_MID_RIGHT_UP:
+      entity = new EntityEnemy(TEXENEMY1, uuid, 15, [](EntityEnemy* enemy) {
+        if (enemy->cycle(1800, 1, 120))
+          enemy->moveBy(-2.5, 0);
+        if (enemy->cycle(1800, 130, 240))
+          enemy->moveBy(2.5, 0);
+        if (enemy->cycle(1800, 250, 900))
+          enemy->moveBy(2.75, -2.75);
+
+        if (enemy->getAge() == 125)
+          enemy->fireBullets(PATTERN_SHOTGUN8X3);
+        if (enemy->getAge() == 245)
+          enemy->fireBullets(PATTERN_SHOTGUN8X3);
+
+        if (enemy->getAge() == 900)  // 15 seconds plus off screen
+          enemy->deleteLater();
+      });
+      break;
+    //***************************************************************************************
+    case ENEMY_WAVE1BASIC:
+      entity = new EntityEnemy(TEXENEMY1, uuid, 25, [](EntityEnemy* enemy) {
+        if (enemy->cycle(240, 0, 100))
+          enemy->moveBy(1, 2);
+        else if (enemy->cycle(240, 140, 240))
+          enemy->moveBy(1, -2);
+        if (enemy->getAge() == 240)
+          enemy->deleteLater();
+        if (enemy->getAge() == 120)
+          enemy->fireBullets(PATTERN_SHOTGUN8X3);
+      });
+      break;
   }
   entity->setPos(loc);
   return entity;
