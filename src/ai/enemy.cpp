a7c9106b38e15c31c70281904ad026b53b498595
spooky <gsw0005@uah.edu>
2021-11-12 13:09:53 -0600

    Wave three start

--- src/ai/enemy.cpp
+++ src/ai/enemy.cpp
@@ -9,10 +9,10 @@ EntityEnemy* spawn(Enemy enemy, UID uuid, const QPointF& loc) {
     case ENEMY_ENEMYTEST1:
       entity =
           new EntityEnemy(TEXTURE_ENEMYTEMP, uuid, 50, [](EntityEnemy* enemy) {
-						if (enemy->cycle(200, 50))
-							enemy->moveTo({150, -300}, 50, QUICK);
-						else if (enemy->cycle(200, 150))
-							enemy->moveTo({-150, -300}, 50);
+            if (enemy->cycle(200, 50))
+              enemy->moveTo({150, -300}, 50, QUICK);
+            else if (enemy->cycle(200, 150))
+              enemy->moveTo({-150, -300}, 50);
           });
       break;
     //***********************************************************************************
@@ -37,12 +37,12 @@ EntityEnemy* spawn(Enemy enemy, UID uuid, const QPointF& loc) {
             else if (enemy->cycle(1800, 271, 800))
               enemy->moveBy(0, -1);
 
-						if (enemy->getAge() == 180)
-							enemy->fireBullets(
-									PATTERN_SHOTGUNX3, AI_BASIC2, TEXTURE_BULLET_ROUND_AQUA);
-						if (enemy->getAge() == 270)
-							enemy->fireBullets(
-									PATTERN_SHOTGUNX3, AI_BASIC2, TEXTURE_BULLET_ROUND_AQUA);
+            if (enemy->getAge() == 180)
+              enemy->fireBullets(
+                  PATTERN_SHOTGUNX3, AI_BASIC2, TEXTURE_BULLET_ROUND_AQUA);
+            if (enemy->getAge() == 270)
+              enemy->fireBullets(
+                  PATTERN_SHOTGUNX3, AI_BASIC2, TEXTURE_BULLET_ROUND_AQUA);
 
             if (enemy->getAge() == 930)  // 15 seconds plus off screen
               enemy->deleteLater();
@@ -59,54 +59,54 @@ EntityEnemy* spawn(Enemy enemy, UID uuid, const QPointF& loc) {
             else if (enemy->cycle(1800, 271, 800))
               enemy->moveBy(0, -1);
 
-						if (enemy->getAge() == 180)
-							enemy->fireBullets(
-									PATTERN_SHOTGUNX3, AI_BASIC2, TEXTURE_BULLET_ROUND_AQUA);
-						if (enemy->getAge() == 270)
-							enemy->fireBullets(
-									PATTERN_SHOTGUNX3, AI_BASIC2, TEXTURE_BULLET_ROUND_AQUA);
+            if (enemy->getAge() == 180)
+              enemy->fireBullets(
+                  PATTERN_SHOTGUNX3, AI_BASIC2, TEXTURE_BULLET_ROUND_AQUA);
+            if (enemy->getAge() == 270)
+              enemy->fireBullets(
+                  PATTERN_SHOTGUNX3, AI_BASIC2, TEXTURE_BULLET_ROUND_AQUA);
             if (enemy->getAge() == 930)  // 15 seconds plus off screen
               enemy->deleteLater();
           });
       break;
     //***************************************************************************************
     case ENEMY_TANKYBANG_UP_DOWN:
-			entity = new EntityEnemy(TEXTURE_ENEMYTEMP,
-															 uuid,
-															 100,
-															 [](EntityEnemy* enemy) {
-				if (enemy->cycle(1800, 1, 175))
-					enemy->moveBy(0, 1.75);
-				if (enemy->cycle(1800, 176, 350))
-					enemy->moveBy(0, -1);
-				if (enemy->cycle(1800, 351, 525))
-					enemy->moveBy(0, 1);
-				if (enemy->cycle(1800, 526, 700))
-					enemy->moveBy(0, -1);
-				if (enemy->cycle(1800, 701, 875))
-					enemy->moveBy(0, 1);
-				if (enemy->cycle(1800, 876, 1050))
-					enemy->moveBy(0, -1);
-				if (enemy->cycle(1800, 1051, 1800))
-					enemy->moveBy(0, -3);
+      entity = new EntityEnemy(TEXTURE_ENEMYTEMP,
+                               uuid,
+                               100,
+                               [](EntityEnemy* enemy) {
+        if (enemy->cycle(1800, 1, 175))
+          enemy->moveBy(0, 1.75);
+        if (enemy->cycle(1800, 176, 350))
+          enemy->moveBy(0, -1);
+        if (enemy->cycle(1800, 351, 525))
+          enemy->moveBy(0, 1);
+        if (enemy->cycle(1800, 526, 700))
+          enemy->moveBy(0, -1);
+        if (enemy->cycle(1800, 701, 875))
+          enemy->moveBy(0, 1);
+        if (enemy->cycle(1800, 876, 1050))
+          enemy->moveBy(0, -1);
+        if (enemy->cycle(1800, 1051, 1800))
+          enemy->moveBy(0, -3);
 
-				if (enemy->cycle(350, 175)) {
-					enemy->fireBullets(
-							PATTERN_SHOTGUNX5, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA, 90);
-					enemy->fireBullets(
-							PATTERN_SHOTGUNX5, AI_BASIC10, TEXTURE_BULLET_ROUND_AQUA, 90);
-					enemy->fireBullets(
-							PATTERN_SHOTGUNX5, AI_BASIC12, TEXTURE_BULLET_ROUND_AQUA, 90);
-					enemy->fireBullets(
-							PATTERN_SHOTGUNX5, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA, 270);
-					enemy->fireBullets(
-							PATTERN_SHOTGUNX5, AI_BASIC10, TEXTURE_BULLET_ROUND_AQUA, 270);
-					enemy->fireBullets(
-							PATTERN_SHOTGUNX5, AI_BASIC12, TEXTURE_BULLET_ROUND_AQUA, 270);
-				}
-				if (enemy->getAge() == 1900)  // 15 seconds plus off screen
-					enemy->deleteLater();
-			});
+        if (enemy->cycle(350, 175)) {
+          enemy->fireBullets(
+              PATTERN_SHOTGUNX5, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA, 90);
+          enemy->fireBullets(
+              PATTERN_SHOTGUNX5, AI_BASIC10, TEXTURE_BULLET_ROUND_AQUA, 90);
+          enemy->fireBullets(
+              PATTERN_SHOTGUNX5, AI_BASIC12, TEXTURE_BULLET_ROUND_AQUA, 90);
+          enemy->fireBullets(
+              PATTERN_SHOTGUNX5, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA, 270);
+          enemy->fireBullets(
+              PATTERN_SHOTGUNX5, AI_BASIC10, TEXTURE_BULLET_ROUND_AQUA, 270);
+          enemy->fireBullets(
+              PATTERN_SHOTGUNX5, AI_BASIC12, TEXTURE_BULLET_ROUND_AQUA, 270);
+        }
+        if (enemy->getAge() == 1900)  // 15 seconds plus off screen
+          enemy->deleteLater();
+      });
       break;
     //****************************************************************************************
     case ENEMY_SIMPLESHOTGUN_MID_LEFT_UP:
@@ -119,13 +119,13 @@ EntityEnemy* spawn(Enemy enemy, UID uuid, const QPointF& loc) {
             if (enemy->cycle(1800, 280, 900))
               enemy->moveBy(-2.75, -2.75);
 
-						if (enemy->getAge() == 155) {
-							enemy->fireBullets(
-									PATTERN_SHOTGUNX3, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA);
-						}
-						if (enemy->getAge() == 275)
-							enemy->fireBullets(
-									PATTERN_SHOTGUNX5, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA);
+            if (enemy->getAge() == 155) {
+              enemy->fireBullets(
+                  PATTERN_SHOTGUNX3, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA);
+            }
+            if (enemy->getAge() == 275)
+              enemy->fireBullets(
+                  PATTERN_SHOTGUNX5, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA);
 
             if (enemy->getAge() == 900)  // 15 seconds plus off screen
               enemy->deleteLater();
@@ -136,24 +136,68 @@ EntityEnemy* spawn(Enemy enemy, UID uuid, const QPointF& loc) {
       entity =
           new EntityEnemy(TEXTURE_ENEMYTEMP, uuid, 40, [](EntityEnemy* enemy) {
             if (enemy->cycle(1800, 1, 120))
-							enemy->moveBy(-5, 0);
+              enemy->moveBy(-5, 0);
             if (enemy->cycle(1800, 160, 240))
               enemy->moveBy(0, -2);
             if (enemy->cycle(1800, 280, 900))
               enemy->moveBy(2.75, -2.75);
 
-						if (enemy->getAge() == 155)
-							enemy->fireBullets(
-									PATTERN_SHOTGUNX3, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA);
-						if (enemy->getAge() == 275)
-							enemy->fireBullets(
-									PATTERN_SHOTGUNX5, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA);
+            if (enemy->getAge() == 155)
+              enemy->fireBullets(
+                  PATTERN_SHOTGUNX3, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA);
+            if (enemy->getAge() == 275)
+              enemy->fireBullets(
+                  PATTERN_SHOTGUNX5, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA);
 
             if (enemy->getAge() == 900)  // 15 seconds plus off screen
               enemy->deleteLater();
           });
       break;
+    //******************************************************************************************
+    case ENEMY_DIVER:
+      entity =
+          new EntityEnemy(TEXTURE_ENEMYTEMP, uuid, 40, [](EntityEnemy* enemy) {
+            if (enemy->cycle(1800, 1, 900))
+              enemy->moveBy(-0.75, 0);
+
+            if (enemy->cycle(50))
+              enemy->fireBullets(
+                  PATTERN_CIRCLE16, AI_FLOWER, TEXTURE_BULLET_ROUND_RED);
+
+            if (enemy->getAge() == 1000)  // 15 seconds plus off screen
+              enemy->deleteLater();
+          });
+      break;
     //***************************************************************************************
+    case ENEMY_LARGE_SLOW_RIGHTTOLEFT:
+      entity = new EntityEnemy(TEXTURE_ENEMYTEMP,
+                               uuid,
+                               80,
+                               [](EntityEnemy* enemy) {
+        if (enemy->cycle(1800, 1, 900))
+          enemy->moveBy(0.75, 0);
+
+        if (enemy->cycle(200))
+          enemy->fireBullet(
+              BulletInfo(), AI_BASIC1, TEXTURE_BULLET_ROUND_RED, 0, {0, 0}, 5);
+      });
+      break;
+    //***************************************************************************************
+    case ENEMY_LARGE_SLOW_LEFTTORIGHT:
+      entity = new EntityEnemy(TEXTURE_ENEMYTEMP,
+                               uuid,
+                               80,
+                               [](EntityEnemy* enemy) {
+        if (enemy->cycle(1800, 1, 900))
+          enemy->moveBy(-0.75, 0);
+
+        if (enemy->cycle(200))
+          enemy->fireBullet(
+              BulletInfo(), AI_BASIC1, TEXTURE_BULLET_ROUND_RED, 0, {0, 0}, 5);
+      });
+      break;
+    //***************************************************************************************
+
     case ENEMY_WAVE1BASIC:
       entity =
           new EntityEnemy(TEXTURE_ENEMYTEMP, uuid, 25, [](EntityEnemy* enemy) {
@@ -163,9 +207,9 @@ EntityEnemy* spawn(Enemy enemy, UID uuid, const QPointF& loc) {
               enemy->moveBy(1, -2);
             if (enemy->getAge() == 240)
               enemy->deleteLater();
-						if (enemy->getAge() == 120)
-							enemy->fireBullets(
-									PATTERN_SHOTGUNX3, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA);
+            if (enemy->getAge() == 120)
+              enemy->fireBullets(
+                  PATTERN_SHOTGUNX3, AI_BASIC8, TEXTURE_BULLET_ROUND_AQUA);
           });
       break;
   }
