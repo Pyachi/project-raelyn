0ad094ee178a1ef332eab339c20468b5a9707d32
Pyachi <snglass2002@hotmail.com>
2021-11-16 14:40:57 -0600

    Fixed brace error

--- src/ai/enemy.cpp
+++ src/ai/enemy.cpp
@@ -25,142 +25,137 @@ const Enemy Enemy::ENEMYTEST2(Texture::ENEMYTEMP, 100, [](EntityEnemy* enemy) {
   if (enemy->getAge() == 60)
     enemy->deleteLater();
 });
-const Enemy Enemy::SIMPLESHOTGUN_TOP_LEFT(
-		Texture::ENEMYTEMP,
-		5,
-		[](EntityEnemy* enemy) {
-			if (enemy->cycle(1800, 1, 150))
-				enemy->moveBy(2, .85);
-			else if (enemy->cycle(1800, 181, 240))
-				enemy->moveBy(1, 0);
-			else if (enemy->cycle(1800, 271, 800))
-				enemy->moveBy(0, -1);
+const Enemy Enemy::SIMPLESHOTGUN_TOP_LEFT(Texture::ENEMYTEMP,
+																					5,
+																					[](EntityEnemy* enemy) {
+	if (enemy->cycle(1800, 1, 150))
+		enemy->moveBy(2, .85);
+	else if (enemy->cycle(1800, 181, 240))
+		enemy->moveBy(1, 0);
+	else if (enemy->cycle(1800, 271, 800))
+		enemy->moveBy(0, -1);
 
-			if (enemy->getAge() == 180) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC2, Texture::ROUNDAQUA, Pattern::SHOTGUN3));
-				SFX::EXPL_LIGHT1.play(25);
-			}
-			if (enemy->getAge() == 270) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC2, Texture::ROUNDAQUA, Pattern::SHOTGUN3));
-				SFX::EXPL_LIGHT1.play(25);
-			}
-			if (enemy->getAge() == 930)  // 15 seconds plus off screen
-				enemy->deleteLater();
-		});
-const Enemy Enemy::SIMPLESHOTGUN_TOP_RIGHT(
-		Texture::ENEMYTEMP,
-		15,
-		[](EntityEnemy* enemy) {
-			if (enemy->cycle(1800, 1, 150))
-				enemy->moveBy(-2, .85);
-			else if (enemy->cycle(1800, 181, 240))
-				enemy->moveBy(-1, 0);
-			else if (enemy->cycle(1800, 271, 800))
-				enemy->moveBy(0, -1);
+	if (enemy->getAge() == 180) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC2, Texture::ROUNDAQUA, Pattern::SHOTGUN3));
+		SFX::EXPL_LIGHT1.play(25);
+	}
+	if (enemy->getAge() == 270) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC2, Texture::ROUNDAQUA, Pattern::SHOTGUN3));
+		SFX::EXPL_LIGHT1.play(25);
+	}
+	if (enemy->getAge() == 930)  // 15 seconds plus off screen
+		enemy->deleteLater();
+});
+const Enemy Enemy::SIMPLESHOTGUN_TOP_RIGHT(Texture::ENEMYTEMP,
+																					 15,
+																					 [](EntityEnemy* enemy) {
+	if (enemy->cycle(1800, 1, 150))
+		enemy->moveBy(-2, .85);
+	else if (enemy->cycle(1800, 181, 240))
+		enemy->moveBy(-1, 0);
+	else if (enemy->cycle(1800, 271, 800))
+		enemy->moveBy(0, -1);
 
-			if (enemy->getAge() == 180) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC2, Texture::ROUNDAQUA, Pattern::SHOTGUN3));
-				SFX::EXPL_LIGHT1.play(25);
-			}
-			if (enemy->getAge() == 270) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC2, Texture::ROUNDAQUA, Pattern::SHOTGUN3));
-				SFX::EXPL_LIGHT1.play(25);
-			}
-			if (enemy->getAge() == 930)  // 15 seconds plus off screen
-				enemy->deleteLater();
-		});
-const Enemy
-		Enemy::TANKYBANG_UP_DOWN(Texture::ENEMYTEMP, 100, [](EntityEnemy* enemy) {
-			if (enemy->cycle(1800, 1, 175))
-				enemy->moveBy(0, 1.5);
-			if (enemy->cycle(1800, 176, 350))
-				enemy->moveBy(0, -.8);
-			if (enemy->cycle(1800, 351, 525))
-				enemy->moveBy(0, .8);
-			if (enemy->cycle(1800, 526, 700))
-				enemy->moveBy(0, -.8);
-			if (enemy->cycle(1800, 701, 875))
-				enemy->moveBy(0, .8);
-			if (enemy->cycle(1800, 876, 1050))
-				enemy->moveBy(0, -.8);
-			if (enemy->cycle(1800, 1051, 1800))
-				enemy->moveBy(0, -3);
+	if (enemy->getAge() == 180) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC2, Texture::ROUNDAQUA, Pattern::SHOTGUN3));
+		SFX::EXPL_LIGHT1.play(25);
+	}
+	if (enemy->getAge() == 270) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC2, Texture::ROUNDAQUA, Pattern::SHOTGUN3));
+		SFX::EXPL_LIGHT1.play(25);
+	}
+	if (enemy->getAge() == 930)  // 15 seconds plus off screen
+		enemy->deleteLater();
+});
+const Enemy Enemy::TANKYBANG_UP_DOWN(Texture::ENEMYTEMP,
+																		 100,
+																		 [](EntityEnemy* enemy) {
+	if (enemy->cycle(1800, 1, 175))
+		enemy->moveBy(0, 1.5);
+	if (enemy->cycle(1800, 176, 350))
+		enemy->moveBy(0, -.8);
+	if (enemy->cycle(1800, 351, 525))
+		enemy->moveBy(0, .8);
+	if (enemy->cycle(1800, 526, 700))
+		enemy->moveBy(0, -.8);
+	if (enemy->cycle(1800, 701, 875))
+		enemy->moveBy(0, .8);
+	if (enemy->cycle(1800, 876, 1050))
+		enemy->moveBy(0, -.8);
+	if (enemy->cycle(1800, 1051, 1800))
+		enemy->moveBy(0, -3);
 
-			if (enemy->cycle(350, 175)) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC8, Texture::ROUNDAQUA, Pattern::SHOTGUN5),
-						90);
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC10, Texture::ROUNDAQUA, Pattern::SHOTGUN5),
-						90);
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC12, Texture::ROUNDAQUA, Pattern::SHOTGUN5),
-						90);
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC8, Texture::ROUNDAQUA, Pattern::SHOTGUN5),
-						270);
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC10, Texture::ROUNDAQUA, Pattern::SHOTGUN5),
-						270);
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC12, Texture::ROUNDAQUA, Pattern::SHOTGUN5),
-						270);
-				SFX::EXPL_MED2.play(25);
-			}
-			if (enemy->getAge() == 1900)  // 15 seconds plus off screen
-				enemy->deleteLater();
-		});
-const Enemy Enemy::SIMPLESHOTGUN_MID_LEFT_UP(
+	if (enemy->cycle(350, 175)) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC8, Texture::ROUNDAQUA, Pattern::SHOTGUN5), 90);
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC10, Texture::ROUNDAQUA, Pattern::SHOTGUN5), 90);
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC12, Texture::ROUNDAQUA, Pattern::SHOTGUN5), 90);
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC8, Texture::ROUNDAQUA, Pattern::SHOTGUN5), 270);
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC10, Texture::ROUNDAQUA, Pattern::SHOTGUN5),
+				270);
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC12, Texture::ROUNDAQUA, Pattern::SHOTGUN5),
+				270);
+		SFX::EXPL_MED2.play(25);
+	}
+	if (enemy->getAge() == 1900)  // 15 seconds plus off screen
+		enemy->deleteLater();
+});
+const Enemy Enemy::SIMPLESHOTGUN_MID_LEFT_UP(Texture::ENEMYTEMP,
                                              55,
-		[](EntityEnemy* enemy) {
-			if (enemy->cycle(1800, 1, 120))
-				enemy->moveBy(5, 0);
-			if (enemy->cycle(1800, 160, 240))
-				enemy->moveBy(0, -2);
-			if (enemy->cycle(1800, 280, 900))
-				enemy->moveBy(-2.75, -2.75);
+																						 [](EntityEnemy* enemy) {
+	if (enemy->cycle(1800, 1, 120))
+		enemy->moveBy(5, 0);
+	if (enemy->cycle(1800, 160, 240))
+		enemy->moveBy(0, -2);
+	if (enemy->cycle(1800, 280, 900))
+		enemy->moveBy(-2.75, -2.75);
 
-			if (enemy->getAge() == 155) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC8, Texture::ROUNDAQUA, Pattern::SHOTGUN3));
-				SFX::EXPL_LIGHT1.play(25);
-			}
-			if (enemy->getAge() == 275) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC8, Texture::ROUNDAQUA, Pattern::SHOTGUN5));
-				SFX::EXPL_LIGHT1.play(25);
-			}
-			if (enemy->getAge() == 900)  // 15 seconds plus off screen
-				enemy->deleteLater();
-		});
-const Enemy Enemy::SIMPLESHOTGUN_MID_RIGHT_UP(
+	if (enemy->getAge() == 155) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC8, Texture::ROUNDAQUA, Pattern::SHOTGUN3));
+		SFX::EXPL_LIGHT1.play(25);
+	}
+	if (enemy->getAge() == 275) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC8, Texture::ROUNDAQUA, Pattern::SHOTGUN5));
+		SFX::EXPL_LIGHT1.play(25);
+	}
+	if (enemy->getAge() == 900)  // 15 seconds plus off screen
+		enemy->deleteLater();
+});
+const Enemy Enemy::SIMPLESHOTGUN_MID_RIGHT_UP(Texture::ENEMYTEMP,
                                               55,
-		[](EntityEnemy* enemy) {
-			if (enemy->cycle(1800, 1, 120))
-				enemy->moveBy(-5, 0);
-			if (enemy->cycle(1800, 160, 240))
-				enemy->moveBy(0, -2);
-			if (enemy->cycle(1800, 280, 900))
-				enemy->moveBy(2.75, -2.75);
+																							[](EntityEnemy* enemy) {
+	if (enemy->cycle(1800, 1, 120))
+		enemy->moveBy(-5, 0);
+	if (enemy->cycle(1800, 160, 240))
+		enemy->moveBy(0, -2);
+	if (enemy->cycle(1800, 280, 900))
+		enemy->moveBy(2.75, -2.75);
 
-			if (enemy->getAge() == 155) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC8, Texture::ROUNDAQUA, Pattern::SHOTGUN3));
-				SFX::EXPL_LIGHT1.play(25);
-			}
-			if (enemy->getAge() == 275) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC8, Texture::ROUNDAQUA, Pattern::SHOTGUN5));
-				SFX::EXPL_LIGHT1.play(25);
-			}
+	if (enemy->getAge() == 155) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC8, Texture::ROUNDAQUA, Pattern::SHOTGUN3));
+		SFX::EXPL_LIGHT1.play(25);
+	}
+	if (enemy->getAge() == 275) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC8, Texture::ROUNDAQUA, Pattern::SHOTGUN5));
+		SFX::EXPL_LIGHT1.play(25);
+	}
 
-			if (enemy->getAge() == 900)  // 15 seconds plus off screen
-				enemy->deleteLater();
-		});
+	if (enemy->getAge() == 900)  // 15 seconds plus off screen
+		enemy->deleteLater();
+});
 const Enemy Enemy::WAVE1BASIC(Texture::ENEMYTEMP, 25, [](EntityEnemy* enemy) {
   if (enemy->cycle(240, 0, 100))
     enemy->moveBy(1, 2);
@@ -184,97 +179,91 @@ const Enemy Enemy::DIVER(Texture::ENEMYTEMP, 40, [](EntityEnemy* enemy) {
   if (enemy->getAge() == 1000)  // 15 seconds plus off screen
     enemy->deleteLater();
 });
-const Enemy Enemy::LARGE_SLOW_LEFTTORIGHT(
-		Texture::ENEMYTEMP,
-		80,
-		[](EntityEnemy* enemy) {
-			if (enemy->cycle(1800, 1, 900))
-				enemy->moveBy(-0.75, 0);
+const Enemy Enemy::LARGE_SLOW_LEFTTORIGHT(Texture::ENEMYTEMP,
+																					80,
+																					[](EntityEnemy* enemy) {
+	if (enemy->cycle(1800, 1, 900))
+		enemy->moveBy(-0.75, 0);
 
-			if (enemy->cycle(150)) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC5, Texture::ROUNDRED, SpawnInfo(), 6));
-				SFX::MISSILE2.play(25);
-			}
-			if (enemy->getAge() == 1000)  // 15 seconds plus off screen
-				enemy->deleteLater();
-		});
-const Enemy Enemy::LARGE_SLOW_RIGHTTOLEFT(
-		Texture::ENEMYTEMP,
-		80,
-		[](EntityEnemy* enemy) {
-			if (enemy->cycle(1800, 1, 900))
-				enemy->moveBy(0.75, 0);
+	if (enemy->cycle(150)) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC5, Texture::ROUNDRED, SpawnInfo(), 6));
+		SFX::MISSILE2.play(25);
+	}
+	if (enemy->getAge() == 1000)  // 15 seconds plus off screen
+		enemy->deleteLater();
+});
+const Enemy Enemy::LARGE_SLOW_RIGHTTOLEFT(Texture::ENEMYTEMP,
+																					80,
+																					[](EntityEnemy* enemy) {
+	if (enemy->cycle(1800, 1, 900))
+		enemy->moveBy(0.75, 0);
 
-			if (enemy->cycle(150)) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC5, Texture::ROUNDRED, SpawnInfo(), 6));
-				SFX::MISSILE2.play(25);
-			}
-			if (enemy->getAge() == 1000)  // 15 seconds plus off screen
-				enemy->deleteLater();
-		});
-const Enemy Enemy::small_fast_RIGHTTOLEFT(
-		Texture::ENEMYTEMP,
-		80,
-		[](EntityEnemy* enemy) {
-			if (enemy->cycle(1800, 1, 900))
-				enemy->moveBy(3, 0);
+	if (enemy->cycle(150)) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC5, Texture::ROUNDRED, SpawnInfo(), 6));
+		SFX::MISSILE2.play(25);
+	}
+	if (enemy->getAge() == 1000)  // 15 seconds plus off screen
+		enemy->deleteLater();
+});
+const Enemy Enemy::small_fast_RIGHTTOLEFT(Texture::ENEMYTEMP,
+																					80,
+																					[](EntityEnemy* enemy) {
+	if (enemy->cycle(1800, 1, 900))
+		enemy->moveBy(3, 0);
 
-			if (enemy->cycle(50)) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC5, Texture::ELLIPSERED, Pattern::CIRCLE6));
-				SFX::EXPL_LIGHT3.play(25);
-			}
-			if (enemy->getAge() == 200)  // 3.75 seconds plus off screen
-				enemy->deleteLater();
-		});
-const Enemy Enemy::small_fast_LEFTTORIGHT(
-		Texture::ENEMYTEMP,
-		80,
-		[](EntityEnemy* enemy) {
-			if (enemy->cycle(1800, 1, 900))
-				enemy->moveBy(-3, 0);
+	if (enemy->cycle(50)) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC5, Texture::ELLIPSERED, Pattern::CIRCLE6));
+		SFX::EXPL_LIGHT3.play(25);
+	}
+	if (enemy->getAge() == 200)  // 3.75 seconds plus off screen
+		enemy->deleteLater();
+});
+const Enemy Enemy::small_fast_LEFTTORIGHT(Texture::ENEMYTEMP,
+																					80,
+																					[](EntityEnemy* enemy) {
+	if (enemy->cycle(1800, 1, 900))
+		enemy->moveBy(-3, 0);
 
-			if (enemy->cycle(50)) {
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC5, Texture::ELLIPSERED, Pattern::CIRCLE6));
-				SFX::EXPL_LIGHT3.play(25);
-			}
-			if (enemy->getAge() == 200)  // 3.75 seconds plus off screen
-				enemy->deleteLater();
-		});
-const Enemy Enemy::small_fast_TOPDOWNTOLEFT(
-		Texture::ENEMYTEMP,
-		80,
-		[](EntityEnemy* enemy) {
-			if (enemy->cycle(900, 1, 450))
-				enemy->moveBy(0, 3);
-			if (enemy->cycle(900, 451, 900))
-				enemy->moveBy(-3, 0);
+	if (enemy->cycle(50)) {
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC5, Texture::ELLIPSERED, Pattern::CIRCLE6));
+		SFX::EXPL_LIGHT3.play(25);
+	}
+	if (enemy->getAge() == 200)  // 3.75 seconds plus off screen
+		enemy->deleteLater();
+});
+const Enemy Enemy::small_fast_TOPDOWNTOLEFT(Texture::ENEMYTEMP,
+																						80,
+																						[](EntityEnemy* enemy) {
+	if (enemy->cycle(900, 1, 450))
+		enemy->moveBy(0, 3);
+	if (enemy->cycle(900, 451, 900))
+		enemy->moveBy(-3, 0);
 
-			if (enemy->cycle(1))
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC5, Texture::ELLIPSERED, Pattern::CIRCLE8));
-			if (enemy->getAge() == 1000)  // 15 seconds plus off screen
-				enemy->deleteLater();
-		});
-const Enemy Enemy::small_fast_TOPDOWNTORIGHT(
-		Texture::ENEMYTEMP,
-		80,
-		[](EntityEnemy* enemy) {
-			if (enemy->cycle(900, 1, 450))
-				enemy->moveBy(0, 3);
-			if (enemy->cycle(1800, 901, 1700))
-				enemy->moveBy(3, 0);
-			if (enemy->cycle(1))
-				enemy->fireBullets(
-						BulletInfo(Bullet::BASIC4, Texture::ELLIPSERED, Pattern::CIRCLE8));
-			if (enemy->getAge() == 1000)  // 15 seconds plus off screen
-				enemy->deleteLater();
-		});
+	if (enemy->cycle(1))
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC5, Texture::ELLIPSERED, Pattern::CIRCLE8));
+	if (enemy->getAge() == 1000)  // 15 seconds plus off screen
+		enemy->deleteLater();
+});
+const Enemy Enemy::small_fast_TOPDOWNTORIGHT(Texture::ENEMYTEMP,
+																						 80,
+																						 [](EntityEnemy* enemy) {
+	if (enemy->cycle(900, 1, 450))
+		enemy->moveBy(0, 3);
+	if (enemy->cycle(1800, 901, 1700))
+		enemy->moveBy(3, 0);
+	if (enemy->cycle(1))
+		enemy->fireBullets(
+				BulletInfo(Bullet::BASIC4, Texture::ELLIPSERED, Pattern::CIRCLE8));
+	if (enemy->getAge() == 1000)  // 15 seconds plus off screen
+		enemy->deleteLater();
+});
 
-const Enemy Enemy::DART1(Texture::ENEMYTEMP, {80}, [](EntityEnemy* enemy) {
+const Enemy Enemy::DART1(Texture::ENEMYTEMP, 80, [](EntityEnemy* enemy) {
   if (enemy->cycle(1000, 10) || enemy->cycle(1000, 510))
     enemy->moveTo({0, 50}, 50, QUICK);
   if (enemy->cycle(1000, 260))
@@ -285,13 +274,15 @@ const Enemy Enemy::DART1(Texture::ENEMYTEMP, {80}, [](EntityEnemy* enemy) {
       enemy->cycle(250, 200)) {
     enemy->fireBullets(
         BulletInfo(Bullet::BASIC10, Texture::ELLIPSEORANGE, SpawnInfo(), 2)
-						<< BulletInfo(Bullet::BASIC6, Texture::ELLIPSEORANGE,
-													SpawnInfo() << SpawnInfo(-10) << SpawnInfo(10), 2),
+						<< BulletInfo(Bullet::BASIC6,
+													Texture::ELLIPSEORANGE,
+													SpawnInfo() << SpawnInfo(-10) << SpawnInfo(10),
+													2),
         enemy->getDirectionOfEntity(Game::getPlayer()));
   }
 });
 
-const Enemy Enemy::DART2(Texture::ENEMYTEMP, {80}, [](EntityEnemy* enemy) {
+const Enemy Enemy::DART2(Texture::ENEMYTEMP, 80, [](EntityEnemy* enemy) {
   if (enemy->cycle(1000, 10) || enemy->cycle(1000, 510))
     enemy->moveTo({75, -200}, 75, QUICK);
   if (enemy->cycle(1000, 260))
@@ -302,13 +293,15 @@ const Enemy Enemy::DART2(Texture::ENEMYTEMP, {80}, [](EntityEnemy* enemy) {
       enemy->cycle(250, 200)) {
     enemy->fireBullets(
         BulletInfo(Bullet::BASIC10, Texture::ELLIPSEORANGE, SpawnInfo(), 2)
-						<< BulletInfo(Bullet::BASIC5, Texture::ELLIPSEORANGE,
-													SpawnInfo() << SpawnInfo(-10) << SpawnInfo(10), 2),
+						<< BulletInfo(Bullet::BASIC5,
+													Texture::ELLIPSEORANGE,
+													SpawnInfo() << SpawnInfo(-10) << SpawnInfo(10),
+													2),
         enemy->getDirectionOfEntity(Game::getPlayer()));
   }
 });
 
-const Enemy Enemy::DART3(Texture::ENEMYTEMP, {80}, [](EntityEnemy* enemy) {
+const Enemy Enemy::DART3(Texture::ENEMYTEMP, 80, [](EntityEnemy* enemy) {
   if (enemy->cycle(1000, 10) || enemy->cycle(1000, 510))
     enemy->moveTo({-75, -200}, 100, QUICK);
   if (enemy->cycle(1000, 260))
@@ -320,13 +313,15 @@ const Enemy Enemy::DART3(Texture::ENEMYTEMP, {80}, [](EntityEnemy* enemy) {
       enemy->cycle(250, 200)) {
     enemy->fireBullets(
         BulletInfo(Bullet::BASIC10, Texture::ELLIPSEORANGE, SpawnInfo(), 2)
-						<< BulletInfo(Bullet::BASIC4, Texture::ELLIPSEORANGE,
-													SpawnInfo() << SpawnInfo(-10) << SpawnInfo(10), 2),
+						<< BulletInfo(Bullet::BASIC4,
+													Texture::ELLIPSEORANGE,
+													SpawnInfo() << SpawnInfo(-10) << SpawnInfo(10),
+													2),
         enemy->getDirectionOfEntity(Game::getPlayer()));
   }
 });
 
-const Enemy Enemy::DART4(Texture::ENEMYTEMP, {50}, [](EntityEnemy* enemy) {
+const Enemy Enemy::DART4(Texture::ENEMYTEMP, 50, [](EntityEnemy* enemy) {
 
   if (enemy->cycle(1000, 10) || enemy->cycle(1000, 510))
     enemy->moveTo({-100, -100}, 100, QUICK);
@@ -348,7 +343,7 @@ const Enemy Enemy::DART4(Texture::ENEMYTEMP, {50}, [](EntityEnemy* enemy) {
   }
 });
 
-const Enemy Enemy::DART5(Texture::ENEMYTEMP, {50}, [](EntityEnemy* enemy) {
+const Enemy Enemy::DART5(Texture::ENEMYTEMP, 50, [](EntityEnemy* enemy) {
 
   if (enemy->cycle(1000, 10) || enemy->cycle(1000, 510))
     enemy->moveTo({100, -100}, 100, QUICK);
@@ -370,7 +365,7 @@ const Enemy Enemy::DART5(Texture::ENEMYTEMP, {50}, [](EntityEnemy* enemy) {
   }
 });
 
-const Enemy Enemy::DART6(Texture::ENEMYTEMP, {50}, [](EntityEnemy* enemy) {
+const Enemy Enemy::DART6(Texture::ENEMYTEMP, 50, [](EntityEnemy* enemy) {
 
   if (enemy->cycle(1000, 10) || enemy->cycle(1000, 510))
     enemy->moveTo({0, -200}, 100, QUICK);
@@ -392,7 +387,7 @@ const Enemy Enemy::DART6(Texture::ENEMYTEMP, {50}, [](EntityEnemy* enemy) {
   }
 });
 
-const Enemy Enemy::SUN(Texture::ENEMYTEMP, {100}, [](EntityEnemy* enemy) {
+const Enemy Enemy::SUN(Texture::ENEMYTEMP, 100, [](EntityEnemy* enemy) {
 
   if (enemy->cycle(1000, 10) || enemy->cycle(1000, 510))
     enemy->moveTo({0, -100}, 100, QUICK);
@@ -405,7 +400,7 @@ const Enemy Enemy::SUN(Texture::ENEMYTEMP, {100}, [](EntityEnemy* enemy) {
   }
 });
 
-const Enemy Enemy::WHEEL(Texture::ENEMYTEMP, {200}, [](EntityEnemy* enemy) {
+const Enemy Enemy::WHEEL(Texture::ENEMYTEMP, 200, [](EntityEnemy* enemy) {
 
   if (enemy->cycle(1000, 10) || enemy->cycle(1000, 510))
     enemy->moveTo({0, -100}, 100, QUICK);
